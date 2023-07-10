2b19d3f59e0dc91d879f8cd41d890f517eca6e88
adrian-cucuet <adrian.cucuet@hotmail.com>
2023-07-10 12:53:21 +0000

    added products view and template

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,27 @@ from django.contrib import admin
 from .models import Product, Category
 
 
-admin.site.register(Product)
-admin.site.register(Category)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'image',
+    )
+
+    ordering = (
+        'name',
+        'category',
+        'price',
+    )
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
