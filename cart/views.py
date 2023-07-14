46369ae629ad8c37b95bb0f7d8694fad6cb5f379
adrian-cucuet <adrian.cucuet@hotmail.com>
2023-07-14 17:18:28 +0000

    Added toasts

--- cart/views.py
+++ cart/views.py
@@ -13,6 +13,7 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """ Add a quantity of the specified product to the shopping cart """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -33,6 +34,7 @@ def add_to_cart(request, item_id):
             cart[item_id] += quantity
         else:
             cart[item_id] = quantity
+            messages.error(request, f'{product.name} added to cart!')
 
     request.session['cart'] = cart
     return redirect(redirect_url)
