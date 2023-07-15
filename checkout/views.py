efaf25dd414a4cd38170f69d3930d7dc66f7c0b0
adrian-cucuet <adrian.cucuet@hotmail.com>
2023-07-15 11:12:04 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -13,7 +13,9 @@ def checkout(request):
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_form
+        'order_form': order_form,
+        'stripe_public_key': 'pk_test_51N9XkEH0nZPjsVFWudaO0OYlY6o7c0gsslK1x65ZKh70kyyyvT9SzjvkSr9T3Wr73cHKQLRHs9Croxe52IOMKDa900rkirCMyV',
+        'client_secret': 'test client secret'
     }
 
     return render(request, template, context)
